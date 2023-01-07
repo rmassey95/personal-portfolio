309d8afae411b44fb2a3e43ff09ac7e90b45277d
Ryan Massey <rmassey.95@gmail.com>
2023-01-07 14:50:15 -0500

    Create personal portfolio page with navbar and footer

---
+++ src/index.js
@@ -0,0 +1,51 @@
+import "./style.css";
+import Email from "./Email";
+
+const linkedinBtn = document.querySelector("#linkedin-btn");
+linkedinBtn.onclick = () => {
+  location.href = "https://www.linkedin.com/in/ryanmassey2573/";
+};
+
+const githubBtn = document.querySelector("#github-btn");
+githubBtn.onclick = () => {
+  location.href = "https://github.com/rmassey95?tab=repositories";
+};
+
+const contactForm = document.querySelector("#contact-form");
+
+function sendEmail(data) {
+  Email.send({
+    SecureToken: "221d3df8-b6f4-4eb8-a1c2-958cda44cd45",
+    To: "rmassey.95@gmail.com",
+    From: "rmassey.95@gmail.com",
+    Subject: `${data.subject}`,
+    Body: `NAME: ${data.name}, EMAIL: ${data.email} 
+    MESSAGE: ${data.msg}`,
+  }).then((message) => alert(message));
+}
+
+contactForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const data = {};
+  data.name = e.target[0].value;
+  data.email = e.target[1].value;
+  data.subject = e.target[2].value;
+  data.msg = e.target[3].value;
+
+  sendEmail(data);
+});
+
+const mobileNavBtn = document.querySelector("#icon");
+
+mobileNavBtn.addEventListener("click", () => {
+  const mobileNavbar = document.querySelector(".mobile-navbar-links");
+  const navbar = document.querySelector(".navbar");
+  const navbarBtn = document.querySelector("#icon");
+  if (mobileNavbar.style.display === "none") {
+    mobileNavbar.style.display = "flex";
+    navbar.classList.add("navbar-mobile");
+  } else {
+    mobileNavbar.style.display = "none";
+    navbar.classList.remove("navbar-mobile");
+  }
+});
